82f39660487e3970163aaaa888a461e1552815b4
Rosendo-Olazabal <tinkerx100pre@gmail.com>
2024-11-21 10:51:44 -0500

    Cambio desde la rama burro

--- index.php
+++ index.php
@@ -9,16 +9,3 @@ $auto1 -> placa = "xel-1345";
 var_dump($auto1);
 ECHO"<br>";
 
-$auto2 = new auto();
-$auto2->marca = "hyundai";
-$auto2-> modelo = "elantra";
-$auto2->color ="blanco";
-$auto2 -> placa ="EF1-W1567";
-var_dump($auto2);
-
-$auto3 = new auto();
-$auto3->marca = "Tesla";
-$auto3-> modelo = "cyber tuck";
-$auto3->color ="plomo";
-$auto3 -> placa ="ERQ-141";
-var_dump($auto2);
