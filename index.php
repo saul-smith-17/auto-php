ac2e4b010fdf9c46871bab7c055b4f6037292e9e
saul-smith-17 <smithochoa08@gmail.com>
2024-11-21 09:24:57 -0500

    cambio deste la ram saul

--- index.php
+++ index.php
@@ -15,3 +15,10 @@ $auto2-> modelo = "elantra";
 $auto2->color ="blanco";
 $auto2 -> placa ="EF1-W1567";
 var_dump($auto2);
+
+$auto3 = new auto();
+$auto3->marca = "Tesla";
+$auto3-> modelo = "cyber tuck";
+$auto3->color ="plomo";
+$auto3 -> placa ="ERQ-141";
+var_dump($auto2);
